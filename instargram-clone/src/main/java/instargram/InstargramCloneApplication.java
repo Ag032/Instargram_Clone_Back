1d24fc8484794b566e66ebadec47b533eefc0b62
agWise <ag@wisecare.co.kr>
2024-08-03 16:38:19 +0900

    FEAT - 프로젝트 초기세팅

---
+++ instargram-clone/src/main/java/instargram/InstargramCloneApplication.java
@@ -0,0 +1,13 @@
+package instargram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstargramCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstargramCloneApplication.class, args);
+    }
+
+}
